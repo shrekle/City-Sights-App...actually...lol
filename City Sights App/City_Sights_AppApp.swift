e198c213f852cce661d6db82b4c455d468c868ff
shrekle <nanonline760@gmail.com>
2022-11-28 14:33:20 -0900

    Initial Commit

---
+++ City Sights App/City_Sights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_Sights_AppApp.swift
+//  City Sights App
+//
+//  Created by adrian garcia on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct City_Sights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
